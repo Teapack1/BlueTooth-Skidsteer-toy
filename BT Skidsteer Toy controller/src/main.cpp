e037acd7f1dd574ca10bcc865a64144469bd4a7a
Teapack1 <ondrej.major@gmail.com>
2023-09-20 23:58:19 +0200

    commit

--- BT Skidsteer Toy controller/src/main.cpp
+++ BT Skidsteer Toy controller/src/main.cpp
@@ -1,4 +1,4 @@
-#include <BluetoothSerial.h>
+#include "BluetoothSerial.h"
 
 BluetoothSerial SerialBT;
 
@@ -11,36 +11,80 @@ BluetoothSerial SerialBT;
 #define JOY2_Y 35
 #define JOY2_Z 36  // Assuming a button for the Z axis
 
+bool isConnected = false;
+
+#define USE_NAME // Comment this to use MAC address instead of a slaveName
+const char *pin = "1234"; // Change this to reflect the pin expected by the real slave BT device
+
+#ifdef USE_NAME
+  String slaveName = "ESP32-BT-Slave"; // Change this to reflect the real name of your slave BT device
+#else
+  String MACadd = "AA:BB:CC:11:22:33"; // This only for printing
+  uint8_t address[6]  = {0xAA, 0xBB, 0xCC, 0x11, 0x22, 0x33}; // Change this to reflect real MAC address of your slave BT device
+#endif
+
+String myName = "ESP32-BT-Master";
+
+void connectToExcavator() {
+  if (!isConnected) {
+    Serial.println("Attempting to connect to Excavator...");
+    #ifdef USE_NAME
+      isConnected = SerialBT.connect(slaveName);
+    #else
+      isConnected = SerialBT.connect(address);
+    #endif
+    if (isConnected) {
+      Serial.println("Connected to Excavator!");
+    } else {
+      Serial.println("Failed to connect. Will retry...");
+      delay(2000);  // Give it some time before the next attempt
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
-  delay(10);
-  SerialBT.begin("ESP32Controller");  // Bluetooth device name
-  SerialBT.connect("Excavator");  // Bluetooth device name
+  delay(1000);  // Delay for stabilization
   
+  SerialBT.begin(myName, true);
+  Serial.printf("The device \"%s\" started in master mode, make sure slave BT device is on!\n", myName.c_str());
+  
+  #ifndef USE_NAME
+    SerialBT.setPin(pin);
+    Serial.println("Using PIN");
+  #endif
+
+  connectToExcavator();
 }
 
 void loop() {
-  //Read the joystick values
+  // Check Bluetooth connection and attempt to reconnect if lost
+  connectToExcavator();
+
+  // Read the joystick values
   int joy1_x_value = analogRead(JOY1_X);
   int joy1_y_value = analogRead(JOY1_Y);
   int joy2_x_value = analogRead(JOY2_X);
   int joy2_y_value = analogRead(JOY2_Y);
   int joy2_z_value = digitalRead(JOY2_Z);
 
-  //map joystick valuest for the motors
+  // Map joystick values for the motors
   int mappedJoy1X = map(joy1_x_value, 0, 4095, -255, 255);
   int mappedJoy1Y = map(joy1_y_value, 0, 4095, -255, 255);
   int mappedJoy2X = map(joy2_x_value, 0, 4095, -255, 255);
   int mappedJoy2Y = map(joy2_y_value, 0, 4095, -255, 255);
 
   // Send the values over BT to excavator.
-  SerialBT.print("M" + String(mappedJoy1X) + "," + String(mappedJoy1Y) + "\n"); //Movement values
-  SerialBT.print("C" + String(mappedJoy2X) + "," + String(mappedJoy2Y) + "," + String(joy2_z_value) + "\n"); //Claw values
+  if (SerialBT.available()) {
+    SerialBT.print("M" + String(mappedJoy1X) + "," + String(mappedJoy1Y) + "\n"); // Movement values
+    SerialBT.print("C" + String(mappedJoy2X) + "," + String(mappedJoy2Y) + "," + String(joy2_z_value) + "\n"); // Claw values
+  } else {
+    isConnected = false;
+  }
 
-  //write the values to serial monitor for debugging
-  Serial.print("M" + String(mappedJoy1X) + "," + String(mappedJoy1Y) + "\n"); //Movement values
-  Serial.print("C" + String(mappedJoy2X) + "," + String(mappedJoy2Y) + "," + String(joy2_z_value) + "\n"); //Claw values
+  // Write the values to serial monitor for debugging
+  Serial.print("M" + String(mappedJoy1X) + "," + String(mappedJoy1Y) + "\n"); // Movement values
+  Serial.print("C" + String(mappedJoy2X) + "," + String(mappedJoy2Y) + "," + String(joy2_z_value) + "\n"); // Claw values
 
-delay(500);
+  delay(500);
 }
-
--- BT Skidsteer Toy/src/main.cpp
+++ BT Skidsteer Toy/src/main.cpp
@@ -1,4 +1,5 @@
 #include <BluetoothSerial.h>
+#include <ESP32Servo.h>
 
 BluetoothSerial SerialBT;
 
@@ -6,100 +7,89 @@ BluetoothSerial SerialBT;
 #define LEFT_MOTOR_FWD 25
 #define LEFT_MOTOR_REV 26
 #define RIGHT_MOTOR_FWD 27
-#define RIGHT_MOTOR_REV 28
-#define ARM_MOTOR_UP 29
-#define ARM_MOTOR_DOWN 30
-#define CLAW_MOTOR_OPEN 31
-#define CLAW_MOTOR_CLOSE 32
+#define RIGHT_MOTOR_REV 32
+
+// Servo pins
+#define ARM_SERVO_PIN 33
+#define CLAW_SERVO_PIN 34
+
+//#define USE_PIN // Uncomment this to use PIN during pairing. The pin is specified on the line below
+const char *pin = "1234"; // Change this to more secure PIN.
+
+String device_name = "Excavator";
+
+void controlMotor(int, int, int);
+
+Servo armServo;
+Servo clawServo;
 
 void setup() {
   Serial.begin(115200);
+  
+  // Initialize motor pins
   pinMode(LEFT_MOTOR_FWD, OUTPUT);
   pinMode(LEFT_MOTOR_REV, OUTPUT); 
   pinMode(RIGHT_MOTOR_FWD, OUTPUT);
   pinMode(RIGHT_MOTOR_REV, OUTPUT);
-  pinMode(ARM_MOTOR_UP, OUTPUT);
-  pinMode(ARM_MOTOR_DOWN, OUTPUT);
-  pinMode(CLAW_MOTOR_OPEN, OUTPUT);
-  pinMode(CLAW_MOTOR_CLOSE, OUTPUT);
   
-  //Setup bluetooth wtih name "Excavator"
-  SerialBT.begin("Excavator");
+  // Attach servos
+  armServo.attach(ARM_SERVO_PIN);
+  clawServo.attach(CLAW_SERVO_PIN);
+  
+  // Setup Bluetooth with name "Excavator"
+  SerialBT.begin(device_name); //Bluetooth device name
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  
+  #ifdef USE_PIN
+    SerialBT.setPin(pin);
+    Serial.println("Using PIN");
+  #endif
 }
 
 void loop() {
-
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
   if (SerialBT.available()) {
     String command = SerialBT.readStringUntil('\n');
     Serial.println(command);
+    
     if (command.startsWith("M")) {
-      // This line of code searches for the position (index) of the first occurrence of a comma , within the command string.
       int commaIndex = command.indexOf(',');
-      //extracts a substring from the command string. This substring starts from the second character (index 1) and goes up to, but does not include, the character at the commaIndex position.
       int leftMotorSpeed = command.substring(1, commaIndex).toInt();
-      //extracts a substring from the command string starting from the character immediately after the comma (hence commaIndex + 1) and goes until the end of the string.
       int rightMotorSpeed = command.substring(commaIndex + 1).toInt();
-      Serial.print("Left motor speed: ");
-      Serial.println(leftMotorSpeed);
-      Serial.print("Right motor speed: ");
-      Serial.println(rightMotorSpeed);
 
-      //Control the motors:
-      if (leftMotorSpeed > 0) {
-        digitalWrite(LEFT_MOTOR_FWD, HIGH);
-        digitalWrite(LEFT_MOTOR_REV, LOW);
-      } else if (leftMotorSpeed < 0) {
-        digitalWrite(LEFT_MOTOR_FWD, LOW);
-        digitalWrite(LEFT_MOTOR_REV, HIGH);
-      } else {
-        digitalWrite(LEFT_MOTOR_FWD, LOW);
-        digitalWrite(LEFT_MOTOR_REV, LOW);
-      }
-      if (rightMotorSpeed > 0) {
-        digitalWrite(RIGHT_MOTOR_FWD, HIGH);
-        digitalWrite(RIGHT_MOTOR_REV, LOW);
-      } else if (rightMotorSpeed < 0) {
-        digitalWrite(RIGHT_MOTOR_FWD, LOW);
-        digitalWrite(RIGHT_MOTOR_REV, HIGH);
-      } else {
-        digitalWrite(RIGHT_MOTOR_FWD, LOW);
-        digitalWrite(RIGHT_MOTOR_REV, LOW);
-      }
-
-      analogWrite(LEFT_MOTOR_FWD, abs(leftMotorSpeed));
-      analogWrite(RIGHT_MOTOR_FWD, abs(rightMotorSpeed));
-
-   
-    } else if (command.startsWith("C")) {
-      // Claw command
-      int commaIndex = command.indexOf(',');
-      int armMotorSpeed = command.substring(1, commaIndex).toInt();
-      int clawMotorSpeed = command.substring(commaIndex + 1).toInt();
-      Serial.print("Arm motor speed: ");
-      Serial.println(armMotorSpeed);
-      Serial.print("Claw motor speed: ");
-      Serial.println(clawMotorSpeed);
+      // Control the wheel motors
+      controlMotor(LEFT_MOTOR_FWD, LEFT_MOTOR_REV, leftMotorSpeed);
+      controlMotor(RIGHT_MOTOR_FWD, RIGHT_MOTOR_REV, rightMotorSpeed);
       
-      if (armMotorSpeed > 0) {
-        digitalWrite(ARM_MOTOR_UP, HIGH);
-        digitalWrite(ARM_MOTOR_DOWN, LOW);
-      } else if (armMotorSpeed < 0) {
-        digitalWrite(ARM_MOTOR_UP, LOW);
-        digitalWrite(ARM_MOTOR_DOWN, HIGH);
-      } else {
-        digitalWrite(ARM_MOTOR_UP, LOW);
-        digitalWrite(ARM_MOTOR_DOWN, LOW);
-      }
-      if (clawMotorSpeed > 0) {
-        digitalWrite(CLAW_MOTOR_OPEN, HIGH);
-        digitalWrite(CLAW_MOTOR_CLOSE, LOW);
-      } else if (clawMotorSpeed < 0) {
-        digitalWrite(CLAW_MOTOR_OPEN, LOW);
-        digitalWrite(CLAW_MOTOR_CLOSE, HIGH);
-      } else {
-        digitalWrite(CLAW_MOTOR_OPEN, LOW);
-        digitalWrite(CLAW_MOTOR_CLOSE, LOW);
-      }
+    } else if (command.startsWith("C")) {
+      int commaIndex = command.indexOf(',');
+      int armPosition = command.substring(1, commaIndex).toInt();
+      int clawPosition = command.substring(commaIndex + 1, commaIndex + 4).toInt(); // Adjusted to read the next value correctly
+
+      // Ensure the servo values are within the valid range
+      armPosition = constrain(armPosition, 0, 180);
+      clawPosition = constrain(clawPosition, 0, 180);
+
+      // Control the servos
+      armServo.write(armPosition);
+      clawServo.write(clawPosition);
+    } else {
+      SerialBT.println("Invalid command received.");
+    }
   }
+}
+
+void controlMotor(int fwdPin, int revPin, int speed) {
+  if (speed > 0) {
+    analogWrite(fwdPin, speed);
+    digitalWrite(revPin, LOW);
+  } else if (speed < 0) {
+    analogWrite(revPin, abs(speed));
+    digitalWrite(fwdPin, LOW);
+  } else {
+    digitalWrite(fwdPin, LOW);
+    digitalWrite(revPin, LOW);
   }
 }
